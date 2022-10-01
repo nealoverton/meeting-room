c7dcd03ff09377db93dd4389fe709cc38657085f
Cora Marcu <cora.marcu@gmail.com>
2022-10-01 21:02:51 +0100

    install FullCalendar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 import "../styles/styles.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Book A Meeting App</h1>
+      <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
     </div>
   );
 }
