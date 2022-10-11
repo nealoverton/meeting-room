a518b4fa8bdfe1950c1935d04843f2babe739639
Cora Marcu <cora.marcu@gmail.com>
2022-10-11 18:27:32 +0100

    start calendar setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import "../styles/styles.js";
 
 function App() {
+  let dayGridLayout = "dayGridDay";
   return (
     <div className="App">
-      <h1>Book A Meeting App</h1>
+      <h1>Book A Meeting Application</h1>
       <FullCalendar
-        defaultView="dayGridMonth"
-        plugins={[dayGridPlugin]}
+        initialView={dayGridLayout}
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        // initialView="timeGridWeek"
         weekends={false}
+        slotDuration="00:30:00"
+        slotMinTime="00:00:00"
       />
     </div>
   );
