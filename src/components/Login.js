a53d387b085839711f3147cc6711bd7816028843
nealoverton <nealjoverton@gmail.com>
2022-10-11 20:02:23 +0100

    allow login credential input

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { profileContext } from "../context";
-import { auth, logInWithEmailAndPassword } from "../firebase";
+import { logInWithEmailAndPassword } from "../firebase";
 import Profile from "../models/Profile";
 
 const Login = () => {
-    const email = "test@test.com";
-    const password = "123456";
+    const { setProfile } = useContext(profileContext);
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
 
-    const {setProfile} = useContext(profileContext);
-    const attemptLogin = async () => {
+    const handleChange = (event, stateSetter) => {
+        stateSetter(event.target.value);
+    }
+
+    const attemptLogin = async (event) => {
+        event.preventDefault();
+
         const uid = await logInWithEmailAndPassword(email, password);
-        const loggedInProfile = new Profile(uid, email)
-        setProfile(loggedInProfile);
+
+        if(uid){
+            const loggedInUserProfile = new Profile(uid, email)
+            setProfile(loggedInUserProfile);
+        }  
     }
 
     return (
         <div>
-            <button onClick={attemptLogin}>Log in</button>
+            <h1>Log in</h1>
+            <form>
+                <label>
+                    Email:
+                    <input type="text" name="email" onChange={(event) => handleChange(event, setEmail)}/>
+                </label>
+                <label>
+                    Password:
+                    <input type="password" name="password" onChange={(event) => handleChange(event, setPassword)}/>
+                </label>
+                <button onClick={attemptLogin}>Log in</button>
+            </form>
+            
         </div>
     )
 }
