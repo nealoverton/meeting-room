7151f5b6a97829d541382873710db67adfa76e94
nealoverton <nealjoverton@gmail.com>
2022-10-12 01:33:17 +0100

    use onauthStatChanged for persistent login

--- src/context.js
+++ src/context.js
@@ -1,12 +1,23 @@
-import { createContext } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
+import { auth } from "./firebase";
 
 const profileContext = createContext();
 
 function ProfileProvider({children}) {
-    const [profile, setProfile] = useState();
-    const value = {profile, setProfile}
-    return <profileContext.Provider value={value}>{children}</profileContext.Provider>
-  }
+  const [profile, setProfile] = useState();
+  const [pending, setPending] = useState(true);
+  const value = {profile, setProfile};
+  
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setProfile(user);
+      setPending(false);
+    })
+  }, [])
 
-  export {ProfileProvider, profileContext};
+  return pending ? <p>Loading...</p> : <profileContext.Provider value={value}>{children}</profileContext.Provider>
+}
+
+export {ProfileProvider, profileContext};
