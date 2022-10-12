ff6eb884fcb0d2fe077febc7b14ee1d252dd584b
nealoverton <nealjoverton@gmail.com>
2022-10-12 14:10:32 +0100

    retrieve profile data for logged in user upon first render

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { authContext } from "../authContext";
 import { logOut } from "../firebase";
+import { getProfileData } from "../firestore";
+import Profile from "../models/Profile";
 
 const Home = () => {
-    const {authentication} = useContext(authContext)
+    const {authentication} = useContext(authContext);
+    const [profile, setProfile] = useState();
+
+    useEffect(() => {
+        const dbData = getProfileData(authentication.uid);
+        const loggedInProfile = new Profile(authentication.uid, authentication.email, dbData.name, dbData.colour)
+        
+        setProfile(loggedInProfile);
+    }, [])
+
+
     return (
         <div>
             <h1>Home</h1>
-            <p>Welcome {authentication.email}</p>
+            {!profile ? <p>Loading...</p> : (
+                <ul>
+                    {Object.keys(profile).map((setting, index) => {
+                        return <li key={index}>{setting + ":  " + profile[setting]}</li>
+                    })}
+                </ul>
+            )}
+            
             <button onClick={() => logOut()}>Log out</button>
         </div>
     )
