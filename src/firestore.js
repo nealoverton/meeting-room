07f1a44bbfb3745085d4e1c7840a27a43841b586
nealoverton <nealjoverton@gmail.com>
2022-10-12 14:09:17 +0100

    get profileData from firestore

--- src/firestore.js
+++ src/firestore.js
@@ -1,15 +1,24 @@
 import {
     doc,
-    setDoc
+    setDoc,
+    getDoc
   } from 'firebase/firestore';
   import { fireStoreDB } from './firebase';
 
-  const addProfile = async (uid, name = 'test name', colour = 'blue') => {
+  const addProfileData = async (uid, name = 'test name', colour = 'blue') => {
     const newProfileRef = doc(fireStoreDB, 'profiles', uid);
   
     return await setDoc(newProfileRef, { name, colour });
   };
 
+  const getProfileData = async (uid) => {
+    const profileRef = doc(fireStoreDB, 'profiles', uid);
+    const profileSnapshot = await getDoc(profileRef);
+
+    return profileSnapshot.data();
+  }
+
   export {
-    addProfile
+    addProfileData,
+    getProfileData
   }
