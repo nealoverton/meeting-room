708472a37ca3c016ecb51c10ced046e8fc326b95
nealoverton <nealjoverton@gmail.com>
2022-10-13 13:05:44 +0100

    obtain all profile data through single exported function

--- src/firestore.js
+++ src/firestore.js
@@ -4,12 +4,19 @@ import {
     getDoc
   } from 'firebase/firestore';
   import { firestoreDB, storage } from './firebase';
-  import { ref, uploadBytes } from 'firebase/storage';
+  import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import Profile from './models/Profile';
 
-  const addProfileData = async (uid, name, colour, hasAvatar) => {
+  const addProfileData = async (uid, name, colour, avatar) => {
+    if(avatar) {
+      const downloadURL = await uploadAvatar(uid, avatar);
+      avatar = downloadURL;
+    } else {
+      avatar = '/images/defaultavatar.png'
+    }
     const newProfileRef = doc(firestoreDB, 'profiles', uid);
   
-    return await setDoc(newProfileRef, { name, colour, hasAvatar });
+    return await setDoc(newProfileRef, { name, colour, avatar });
   };
 
   const getProfileData = async (uid) => {
@@ -19,13 +26,30 @@ import {
     return profileSnapshot.data();
   }
 
-  const uploadImage = (uid, file) => {
+  const createProfileFromUser = async ({uid, email}) => {
+    const {name, colour, avatar} = await getProfileData(uid);
+    
+    const newProfile = new Profile(uid, email, name, colour, avatar);
+
+    return newProfile;
+  }
+
+  const uploadAvatar = async (uid, file) => {
     const storageRef = ref(storage, uid)
-    return uploadBytes(storageRef, file);
+    await uploadBytes(storageRef, file);
+    return await getDownloadURL(storageRef);
+  }
+
+  const getAvatar = async (uid) => {
+    const storageRef = ref(storage, uid)
+    const avatarSnapshot = await getDoc(storageRef);
+
+    return avatarSnapshot.data();
   }
 
   export {
     addProfileData,
-    getProfileData,
-    uploadImage
+    createProfileFromUser,
+    uploadAvatar,
+    getAvatar
   }
