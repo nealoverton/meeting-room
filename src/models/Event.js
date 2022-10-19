44500ffcf9193696033e4a4790d08746969017c4
nealoverton <nealjoverton@gmail.com>
2022-10-19 13:59:45 +0100

    include eventID in Event object for updating

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,9 +1,10 @@
 class Event {
-  constructor(title, start, end, color) {
+  constructor(title, start, end, color, eventID) {
     this.title = title;
     this.start = start;
     this.end = end;
     this.color = color;
+    this.eventID = eventID
     this.allDay = false;
   }
 }
