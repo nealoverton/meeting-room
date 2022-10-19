ca7570bf99fc9f0335a506e244887776016f118b
nealoverton <nealjoverton@gmail.com>
2022-10-19 14:23:54 +0100

    include editable prop in Event object

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,11 +1,13 @@
 class Event {
-  constructor(title, start, end, color, eventID) {
+  constructor(title, start, end, color, eventID, editable = false) {
     this.title = title;
     this.start = start;
     this.end = end;
     this.color = color;
-    this.eventID = eventID
+    this.eventID = eventID;
+    this.editable = editable;
     this.allDay = false;
+    
   }
 }
 
