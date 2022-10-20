58a96dd75a36a460b1a5af2f17fa30f5e7752640
nealoverton <nealjoverton@gmail.com>
2022-10-20 15:06:32 +0100

    disabled login button until input values checked for basic validity

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { logInWithEmailAndPassword } from "../firebase";
 
 const Login = () => {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailIsValid, setEmailIsValid] = useState(false);
+    const [passwordIsValid, setPasswordIsValid] = useState(false);
     const navigate = useNavigate();
-
-    const handleChange = (event, stateSetter) => {
-        stateSetter(event.target.value);
-    }
 
     const attemptLogin = async (event) => {
         event.preventDefault();
@@ -18,19 +16,35 @@ const Login = () => {
         navigate("/");
     }
 
+    useEffect(() => {
+        if(password.length >= 6){
+            setPasswordIsValid(true);
+        } else{
+            setPasswordIsValid(false);
+        } 
+
+        const validEmail = /\S+@\S+\.\S+/
+
+        if(validEmail.test(email)){
+            setEmailIsValid(true);
+        }else{
+            setEmailIsValid(false)
+        } 
+    }, [email, password])
+
     return (
         <div>
             <h1>Log in</h1>
             <form>
                 <label>
                     Email:
-                    <input type="text" name="email" onChange={(event) => handleChange(event, setEmail)}/>
+                    <input type="text" name="email" onChange={(event) => setEmail(event.target.value)}/>
                 </label>
                 <label>
                     Password:
-                    <input type="password" name="password" onChange={(event) => handleChange(event, setPassword)}/>
+                    <input type="password" name="password" onChange={(event) => setPassword(event.target.value)}/>
                 </label>
-                <button onClick={attemptLogin}>Log in</button>
+                <button onClick={attemptLogin} disabled={!(emailIsValid && passwordIsValid)}>Log in</button>
             </form>
             <Link to="/register">New around here? Create an account</Link>
         </div>
