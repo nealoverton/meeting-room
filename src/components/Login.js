225777e85a841b7a4485b4ff30f53a899d4605f3
nealoverton <nealjoverton@gmail.com>
2022-10-20 15:55:08 +0100

    disable submit button until user has followed described input patterns

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,42 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [emailIsValid, setEmailIsValid] = useState(false);
     const [passwordIsValid, setPasswordIsValid] = useState(false);
+    const [submitIsDisabled, setSubmitIsDisabled] = useState(true);
+    const [showEmailError, setShowEmailError] = useState(false);
+    const [showPasswordError, setShowPasswordError] = useState(false);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        setSubmitIsDisabled(!(emailIsValid && passwordIsValid))
+        setLoading(false);
+    }, [])
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+
+        const validEmailPattern = /\S+@\S+\.\S+/
+
+        if(validEmailPattern.test(event.target.value)){
+            setEmailIsValid(true);
+            setSubmitIsDisabled(!passwordIsValid);
+        }else{
+            setEmailIsValid(false);
+            setSubmitIsDisabled(true);
+        }
+    }
+
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
+
+        if(event.target.value.length >= 6){
+            setPasswordIsValid(true);
+            setSubmitIsDisabled(!emailIsValid);
+        }else{
+            setPasswordIsValid(false);
+            setSubmitIsDisabled(true);
+        }
+    }
 
     const attemptLogin = async (event) => {
         event.preventDefault();
@@ -16,35 +51,21 @@ const Login = () => {
         navigate("/");
     }
 
-    useEffect(() => {
-        if(password.length >= 6){
-            setPasswordIsValid(true);
-        } else{
-            setPasswordIsValid(false);
-        } 
-
-        const validEmail = /\S+@\S+\.\S+/
-
-        if(validEmail.test(email)){
-            setEmailIsValid(true);
-        }else{
-            setEmailIsValid(false)
-        } 
-    }, [email, password])
-
     return (
         <div>
             <h1>Log in</h1>
             <form>
                 <label>
                     Email:
-                    <input type="text" name="email" onChange={(event) => setEmail(event.target.value)}/>
+                    <input type="text" name="email" onChange={handleChangeEmail} onBlur={()=>{setShowEmailError(!emailIsValid)}}/>
+                    {showEmailError? <p>this doesn't look right (eg. somebody@somewhere.com)</p> : <></>}
                 </label>
                 <label>
                     Password:
-                    <input type="password" name="password" onChange={(event) => setPassword(event.target.value)}/>
+                    <input type="password" name="password" onChange={handleChangePassword} onBlur={()=>{setShowPasswordError(!passwordIsValid)}}/>
+                    {showPasswordError? <p>password must be at least 6 characters long</p> : <></>}
                 </label>
-                <button onClick={attemptLogin} disabled={!(emailIsValid && passwordIsValid)}>Log in</button>
+                {loading ? <p>Loading...</p> : <button onClick={attemptLogin} disabled={submitIsDisabled}>Log in</button>}
             </form>
             <Link to="/register">New around here? Create an account</Link>
         </div>
