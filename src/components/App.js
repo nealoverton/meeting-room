641f6f9b3e8b8326342b0c5c1fa21d5b4ce987b1
nealoverton <nealjoverton@gmail.com>
2022-10-27 07:49:28 +0100

    use ProtectedLogin to forbid navigation to login/register when already logged in

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from "./Home";
 import ProtectedRoute from "./ProtectedRoute";
 import Login from "./Login";
 import Register from "./Register";
+import ProtectedLogin from "./ProtectedLogin";
 
 function App() {
 
@@ -13,8 +14,17 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <Routes>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
+              <Route path='/login' element={
+                <ProtectedLogin>
+                  <Login/>
+                </ProtectedLogin>
+              }/>
+
+              <Route path='/register' element={
+                <ProtectedLogin>
+                  <Register/>
+                </ProtectedLogin>
+              }/>            
 
             <Route path='/' element={
               <ProtectedRoute>
